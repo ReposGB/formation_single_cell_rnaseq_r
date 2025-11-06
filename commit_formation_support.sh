70c7c673ae300d9ad33f17abc381348a0b4c55ac
Ghislain BIDAUT <ghislain.bidaut@inserm.fr>
2025-11-06 11:17:26 +0100

    update

--- commit_formation_support.sh
+++ commit_formation_support.sh
@@ -2,6 +2,7 @@
 
 cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/index.html .
 cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation.html .
+cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation_files .
 
 
 cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/*png .
