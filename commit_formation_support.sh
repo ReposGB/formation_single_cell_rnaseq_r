d8f536e936835a3ea16d4c8ee8f66318b94995f8
Ghislain BIDAUT <ghislain.bidaut@inserm.fr>
2026-09-18 11:41:15 +0200

    update

--- commit_formation_support.sh
+++ commit_formation_support.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/index.html .
-cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/figures-intro-R .
-cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation.html .
-cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation_files .
+cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/index.html .
+cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/figures-intro-R .
+cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation.html .
+cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_Formation_files .
 
-cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_R_RStudio.html .
-cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_R_RStudio_files .
+cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_R_RStudio.html .
+cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Introduction_R_RStudio_files .
 
-cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/TD_Introduction_R_RStudio.html .
+cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/TD_Introduction_R_RStudio.html .
 
 
-#cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Analyse_scRNA-seq_Seurat.html .
-#cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/Analyse_scRNA-seq_Seurat_files  .
-#cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/figures-seurat .
-#cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/figures_RNA_seq  .
+#cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Analyse_scRNA-seq_Seurat.html .
+#cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/Analyse_scRNA-seq_Seurat_files  .
+#cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/figures-seurat .
+#cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/figures_RNA_seq  .
 
-#cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat.html .
-#cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_files .
+#cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat.html .
+#cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_files .
 
-#cp -p $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_Advanced.html .
-#cp -rp $HOME/CRCMBox/Cours/Formation_NGS_AMU_2025/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_Advanced_files .
+#cp -p $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_Advanced.html .
+#cp -rp $HOME/crcmcloud/Cours/Formation_NGS_AMU_2026/Module2_Formation_R_SingleCellRNASeq/docs/TD_scRNA-seq_Seurat_Advanced_files .
 
 
 
